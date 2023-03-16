ec47100636fe228ad17944679ea6b7a5794d5d4d
Richard Wang <62804488+ricky9w@users.noreply.github.com>
2023-03-16 23:58:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone --single-branch --depth 1 https://github.com/vernesong/OpenClash package/luci-app-openclash
